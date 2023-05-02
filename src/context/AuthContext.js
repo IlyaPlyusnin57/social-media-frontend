8d16ed56667163b436036f4390c8d561c73a1e7d
= <=>
2023-05-02 13:56:15 -0400

    remove comment for socket connection

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -37,46 +37,46 @@ function AuthContextProvider({ children }) {
 
   console.log({ onlineFriends: state.onlineFriends });
 
-  // useEffect(() => {
-  //   if (state.user?._id) {
-  //     socket.current = io("ws://localhost:8900", {
-  //       query: { userId: state.user._id },
-  //     });
+  useEffect(() => {
+    if (state.user?._id) {
+      socket.current = io("ws://localhost:8900", {
+        query: { userId: state.user._id },
+      });
 
-  //     dispatch({ type: "SET_SOCKET", payload: socket.current });
+      dispatch({ type: "SET_SOCKET", payload: socket.current });
 
-  //     console.log("emitting the getuserId");
-  //     socket.current.emit("getUserId", state.user._id);
+      console.log("emitting the getuserId");
+      socket.current.emit("getUserId", state.user._id);
 
-  //     socket.current.on("getUsers", (friends) => {
-  //       console.log({ friends });
-  //       if (Array.isArray(friends)) {
-  //         const mapFriends = new Map(
-  //           friends.map((item, i) => {
-  //             return [item, i];
-  //           })
-  //         );
+      socket.current.on("getUsers", (friends) => {
+        console.log({ friends });
+        if (Array.isArray(friends)) {
+          const mapFriends = new Map(
+            friends.map((item, i) => {
+              return [item, i];
+            })
+          );
 
-  //         console.log(mapFriends);
-  //         dispatch({ type: "UPDATE_ONLINE_FRIENDS", payload: mapFriends });
-  //       }
-  //     });
+          console.log(mapFriends);
+          dispatch({ type: "UPDATE_ONLINE_FRIENDS", payload: mapFriends });
+        }
+      });
 
-  //     socket.current.on("getUser", (userId) => {
-  //       console.log({ receiveId: userId });
-  //       dispatch({ type: "ADD_ONLINE_FRIEND", payload: userId });
-  //     });
+      socket.current.on("getUser", (userId) => {
+        console.log({ receiveId: userId });
+        dispatch({ type: "ADD_ONLINE_FRIEND", payload: userId });
+      });
 
-  //     socket.current.on("removeUser", (userId) => {
-  //       console.log({ removeId: userId });
-  //       dispatch({ type: "REMOVE_ONLINE_FRIEND", payload: userId });
-  //     });
+      socket.current.on("removeUser", (userId) => {
+        console.log({ removeId: userId });
+        dispatch({ type: "REMOVE_ONLINE_FRIEND", payload: userId });
+      });
 
-  //     socket.current.on("getMessageNotification", (message) => {
-  //       dispatch({ type: "SET_MESSAGE_NOTIFICATION", payload: message });
-  //     });
-  //   }
-  // }, [state.user?._id]);
+      socket.current.on("getMessageNotification", (message) => {
+        dispatch({ type: "SET_MESSAGE_NOTIFICATION", payload: message });
+      });
+    }
+  }, [state.user?._id]);
 
   return (
     <AuthContext.Provider
