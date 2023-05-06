4be48e706ce6ef8bc59141adae27ea9de1e72fa6
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-05-05 21:58:05 -0400

    Add time prop to Message component
    For the time prop, there also have to be a getTime function to format
    the time displaying the time in AM/PM formate. The time is inserted
    after the message so the message is moved to its own div called
    message-content. The scss file is updated to format the message
    specifying a minimum and a maximum width while setting the width to its
    max content, and also setting the wrap to break the words. There are
    also classes for the time and the message-content to format them
    accordingly

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -2,14 +2,26 @@ import "./Message.scss";
 
 import { forwardRef, memo } from "react";
 
+function getTime(time) {
+  const newTime = new Date(time);
+  return new Intl.DateTimeFormat("default", {
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+  }).format(newTime);
+}
+
 const Message = memo(
-  forwardRef(({ message, own }, ref) => {
+  forwardRef(({ message, own, time }, ref) => {
     return (
       <div
         ref={ref}
         className={own ? "message-container own" : "message-container"}
       >
-        <div className={own ? "message own" : "message"}>{message}</div>
+        <div className={own ? "message own" : "message"}>
+          <div className="message-content">{message}</div>
+          <section className="time">{getTime(time)}</section>
+        </div>
       </div>
     );
   })
