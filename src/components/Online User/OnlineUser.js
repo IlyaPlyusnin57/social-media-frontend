10074d1ef1356576b746c806609cad0036b84f4e
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-05-05 21:36:39 -0400

    Add backgroundColor prop and create style object
    If the backgroundColor prop is present, use it to set the border
    property on the object and pass it the span circle to change the color
    of the border

--- src/components/Online User/OnlineUser.js
+++ src/components/Online User/OnlineUser.js
@@ -1,15 +1,19 @@
 import "./OnlineUser.scss";
 import { useAuth } from "../../context/AuthContext";
 
-function OnlineUser({ profilePicture, userId }) {
+function OnlineUser({ profilePicture, userId, backgroundColor }) {
   const { onlineFriends } = useAuth();
+
+  const style = {};
+
+  if (backgroundColor) style["border"] = `3px solid ${backgroundColor}`;
 
   const isOnline = onlineFriends?.has(userId);
 
   return (
     <div className="online-user">
       <img src={profilePicture} alt="" />
-      {isOnline && <span className="circle"></span>}
+      {isOnline && <span className="circle" style={style}></span>}
     </div>
   );
 }
