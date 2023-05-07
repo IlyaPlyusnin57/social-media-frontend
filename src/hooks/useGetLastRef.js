bf125da09e51b812dc795d28a688999c5e0d8a31
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-05-07 14:05:20 -0400

    Add useGetLastRef hook
    This hook returns a ref that, when attached to an element, sets the
    nextId if the element is intersecting and there is a nextPage and nextId
    is present

---
+++ src/hooks/useGetLastRef.js
@@ -0,0 +1,25 @@
+import { useCallback, useRef } from "react";
+
+export function useGetLastRef(isFetching, hasNextPage, nextId, setLastId) {
+  const intObserver = useRef();
+
+  const lastRef = useCallback(
+    (element) => {
+      if (isFetching) return;
+
+      if (intObserver.current) intObserver.current.disconnect();
+
+      intObserver.current = new IntersectionObserver((elements) => {
+        if (elements[0].isIntersecting && hasNextPage) {
+          console.log("We are near the last user!");
+          if (nextId.current) setLastId(nextId.current);
+        }
+      });
+
+      if (element) intObserver.current.observe(element);
+    },
+    [isFetching, hasNextPage, setLastId, nextId]
+  );
+
+  return lastRef;
+}
