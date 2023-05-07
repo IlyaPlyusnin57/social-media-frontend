77d8a816fff54a7c92e76c3160d160449632bd84
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-05-07 14:06:35 -0400

    Add useNextQuery hook
    This hook fetches the next set of query results and updates the results
    state variable with the new data. It also sets the hasNextPage variable
    to indicate whether there are more results available and sets the
    nextUserId ref to the last user's ID, if there is such an ID, which can
    be used for the next query call.
    
    Finally, it returns all of this data to the component that
    calls the hook

---
+++ src/hooks/useNextQuery.js
@@ -0,0 +1,31 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState, useRef } from "react";
+import useAxiosConfig from "../api/useAxiosConfig";
+
+export function useNextQuery(queryKeys, queryFunction, queryFunctionOptions) {
+  const [results, setResults] = useState([]);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const nextUserId = useRef(null);
+  const api = useAxiosConfig();
+
+  const { isError, error, isFetching } = useQuery({
+    queryKey: [...queryKeys],
+    queryFn: () => queryFunction(api, queryFunctionOptions),
+    refetchOnWindowFocus: false,
+    onSuccess: (data) => {
+      setResults((results) => [...results, ...data]);
+      setHasNextPage(data.length === 10);
+      nextUserId.current =
+        data.length === 10 ? data[data.length - 1]._id : null;
+    },
+  });
+
+  return {
+    isError,
+    error,
+    isFetching,
+    results,
+    hasNextPage,
+    nextUserId,
+  };
+}
