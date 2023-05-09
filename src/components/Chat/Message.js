f8a042cbca58ec6a8737f3b4dd34f5e7f247424c
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-05-08 23:24:07 -0400

    Add a ref to message div and to message-content div
    Ref to the message div uses IntersectionObserver to see if the message
    is intersecting.
    
    Ref to the message-content div is used to access the element in the DOM
    to retrieve its y position.
    
    When the message div is intersecting, we check the y position of the
    message-content div and if it is below the current-conversation-month
    div, then we update the current-conversation-month div to the month
    and day of the message

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -21,13 +21,45 @@ function getMonthAndDay(time) {
 
 const Message = memo(
   forwardRef(({ message, own, time }, ref) => {
+    const intObserver = useRef();
+    const refPosition = useRef();
+
+    const messageRef = useCallback(
+      (element) => {
+        if (intObserver.current) intObserver.current.disconnect();
+
+        intObserver.current = new IntersectionObserver((elements) => {
+          if (elements[0].isIntersecting && refPosition.current) {
+            const lastMessageDatePosition = document.querySelector(
+              ".current-conversation-month"
+            );
+
+            const messagePositionY =
+              refPosition.current.getBoundingClientRect().y;
+            const lastMessageDatePositionY = document
+              .querySelector(".current-conversation-month")
+              ?.getBoundingClientRect().y;
+
+            if (messagePositionY > lastMessageDatePositionY) {
+              lastMessageDatePosition.innerHTML = getMonthAndDay(time);
+            }
+          }
+        });
+
+        if (element) intObserver.current.observe(element);
+      },
+      [time]
+    );
+
     return (
       <div
         ref={ref}
         className={own ? "message-container own" : "message-container"}
       >
-        <div className={own ? "message own" : "message"}>
-          <div className="message-content">{message}</div>
+        <div className={own ? "message own" : "message"} ref={messageRef}>
+          <div className="message-content" ref={refPosition}>
+            {message}
+          </div>
           <section className="time">{getTime(time)}</section>
         </div>
       </div>
