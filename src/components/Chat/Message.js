e1ca5f89bebc4c931f2f6c8abd592c9ad328e8dd
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-05-16 23:47:12 -0400

    Remove unnecessary imports
    Add separate prop
    
    Add custom data attribute to message-container div so it could be
    targeted later from the Conversation componenent
    
    Add date-separator div to display the date when the separate prop is
    true. This divides the messages when their date changes
    
    Remove ref refPosition as it is no longer needed

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -1,20 +1,25 @@
 import "./Message.scss";
 
-import { forwardRef, memo, useRef, useCallback } from "react";
+import { forwardRef, memo } from "react";
 import { getMonthAndDay } from "../../helper_functions/monthAndDay";
 import { getTime } from "../../helper_functions/getTime";
 
 const Message = memo(
-  forwardRef(({ message, own, time }, ref) => {
+  forwardRef(({ message, own, time, separate }, ref) => {
     return (
       <div
         ref={ref}
         className={own ? "message-container own" : "message-container"}
+        data-time={getMonthAndDay(time)}
       >
-        <div className={own ? "message own" : "message"} ref={messageRef}>
-          <div className="message-content" ref={refPosition}>
-            {message}
-          </div>
+        {separate && (
+          <section className="date-separator-wrapper">
+            <div className="date-separator">{getMonthAndDay(time)}</div>
+          </section>
+        )}
+
+        <div className={own ? "message own" : "message"}>
+          <div className="message-content">{message}</div>
           <section className="time">{getTime(time)}</section>
         </div>
       </div>
