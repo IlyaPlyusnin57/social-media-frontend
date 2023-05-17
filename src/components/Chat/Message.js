fffb07d9ae75ae3ac89287ab7ad3da02e8266af7
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-05-16 23:16:44 -0400

    Remove getTime and getMonthAndDay functions and import them instead

--- src/components/Chat/Message.js
+++ src/components/Chat/Message.js
@@ -1,23 +1,8 @@
 import "./Message.scss";
 
 import { forwardRef, memo, useRef, useCallback } from "react";
-
-function getTime(time) {
-  const newTime = new Date(time);
-  return new Intl.DateTimeFormat("default", {
-    hour12: true,
-    hour: "numeric",
-    minute: "numeric",
-  }).format(newTime);
-}
-
-function getMonthAndDay(time) {
-  const date = new Date(time);
-  return new Intl.DateTimeFormat("default", {
-    month: "long",
-    day: "numeric",
-  }).format(date);
-}
+import { getMonthAndDay } from "../../helper_functions/monthAndDay";
+import { getTime } from "../../helper_functions/getTime";
 
 const Message = memo(
   forwardRef(({ message, own, time }, ref) => {
