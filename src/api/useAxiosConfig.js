c30ba5a09c80d40c8359adcab2a5c16d2a6d879d
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-06-22 15:36:10 -0400

    Remove main_api and import it instead
    Also remove console logs

--- src/api/useAxiosConfig.js
+++ src/api/useAxiosConfig.js
@@ -2,17 +2,10 @@ import { isExpired } from "react-jwt";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { refreshToken } from "../helper_functions/refreshToken";
-import axios from "axios";
-
-const main_api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-  withCredentials: true,
-});
+import { main_api } from "./axiosDefaultSettings";
 
 export default function useAxiosConfig(user, dispatch, socket) {
   const navigate = useNavigate();
-
-  console.log({ useAxiosConfig: user });
 
   useEffect(() => {
     //main_api.defaults.headers = { authorization: `Bearer ${user.accessToken}` };
--- src/api/useAxiosConfig2.js
+++ src/api/useAxiosConfig2.js
@@ -3,18 +3,11 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { refreshToken } from "../helper_functions/refreshToken";
 import { useAuth } from "../context/AuthContext";
-import axios from "axios";
-
-const main_api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-  withCredentials: true,
-});
+import { main_api } from "./axiosDefaultSettings";
 
 export default function useAxiosConfig2() {
   const navigate = useNavigate();
   const { user, dispatch, socket } = useAuth();
-
-  console.log({ useAxiosConfig: user });
 
   useEffect(() => {
     //main_api.defaults.headers = { authorization: `Bearer ${user.accessToken}` };
