dac7e05be62e6859323d8ce1d748577249cb070f
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-06-24 18:32:07 -0400

    Add navigation to OnlineUser component
    When user profile picture is clicked, the app is redirected to that
    user's home page

--- src/components/Online User/OnlineUser.js
+++ src/components/Online User/OnlineUser.js
@@ -1,17 +1,27 @@
 import "./OnlineUser.scss";
 import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import useAxiosConfig2 from "../../api/useAxiosConfig2";
+import { getUser } from "../../apiCalls";
 
 function OnlineUser({ profilePicture, userId, backgroundColor }) {
   const { onlineFriends } = useAuth();
+  const navigate = useNavigate();
+  const api = useAxiosConfig2();
 
   const style = {};
+
+  async function navigateToUser() {
+    const user = await getUser(api, userId);
+    navigate("/search-profile", { state: user });
+  }
 
   if (backgroundColor) style["border"] = `3px solid ${backgroundColor}`;
 
   const isOnline = onlineFriends?.has(userId);
 
   return (
-    <div className="online-user">
+    <div className="online-user" onClick={navigateToUser}>
       <img src={profilePicture} alt="" />
       {isOnline && <span className="circle" style={style}></span>}
     </div>
