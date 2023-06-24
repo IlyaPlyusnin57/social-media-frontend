160d830fb37bcf94960329fcd114a56a7bc25bb2
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-06-24 18:24:52 -0400

    Remove console log

--- src/helper_functions/refreshToken.js
+++ src/helper_functions/refreshToken.js
@@ -2,8 +2,6 @@ import { refresh_api } from "../api/axiosDefaultSettings";
 import { handleLogout } from "../apiCalls";
 
 export async function refreshToken(user, socket, dispatch, navigate) {
-  console.log({ userInRefreshToken: user });
-
   try {
     const res = await refresh_api.post("/auth/refresh", {
       userId: user._id,
