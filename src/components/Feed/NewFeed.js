496e2bd5d008c0f8680d8b39c35664af04f5c2ea
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-05 23:43:22 -0400

    Define a query that returns subscriptions' posts
    This specific implementation of the algorithm returns posts that were
    made in the last 3 days

--- src/components/Feed/NewFeed.js
+++ src/components/Feed/NewFeed.js
@@ -4,43 +4,81 @@ import { useAuth } from "../../context/AuthContext";
 import { useQuery } from "@tanstack/react-query";
 import { getUsersFeedForADay } from "../../apiCalls";
 import useAxiosConfig2 from "../../api/useAxiosConfig2";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Post from "./Timeline/Post";
+import { useGetLastRef2 } from "../../hooks/useGetLastRef2";
 
 function NewFeed() {
   const { user } = useAuth();
   const api = useAxiosConfig2();
+  const [posts, setPosts] = useState([]);
+  const [lastPostId, setLastPostId] = useState(null);
+
+  const followIndex = useRef(0);
+  const [userId, setUserId] = useState(user.following[followIndex]);
 
   const currentUser = useRef({
-    userId: user.following[0],
+    userId: user.following[followIndex.current],
     currDay: 0,
     prevDay: 1,
+    lastPostId: null,
+    fetchNext: false,
+    hasFinished: false,
   });
 
-  const {
-    isLoading,
-    isError,
-    data: posts,
-    error,
-  } = useQuery({
-    queryKey: ["user-feed-for-a-day", currentUser.current.userId],
+  const { isLoading, isFetching, isError, error } = useQuery({
+    queryKey: ["user-feed-for-a-day", lastPostId, userId],
+    refetchOnWindowFocus: false,
     queryFn: () =>
       getUsersFeedForADay(api, currentUser.current.userId, {
         currDay: currentUser.current.currDay,
         prevDay: currentUser.current.prevDay,
+        lastPostId: currentUser.current.lastPostId,
       }),
+    onSuccess: (data) => {
+      if (data.length === 10) {
+        currentUser.current.lastPostId = data[data.length - 1]._id;
+        currentUser.current.fetchNext = false;
+      } else {
+        followIndex.current++;
+
+        if (followIndex.current % user.following.length === 0) {
+          currentUser.current.currDay++;
+          currentUser.current.prevDay++;
+        }
+
+        if (currentUser.current.prevDay === 4) {
+          currentUser.current.hasFinished = true;
+        }
+
+        currentUser.current.lastPostId = null;
+        currentUser.current.fetchNext = true;
+        currentUser.current.userId =
+          user.following[followIndex.current % user.following.length];
+      }
+
+      setPosts((prev) => [...prev, ...data]);
+    },
   });
 
-  if (isLoading) {
-    return <span>Loading...</span>;
-  }
+  const lastPostRef = useGetLastRef2(
+    isFetching,
+    currentUser.current.lastPostId,
+    setLastPostId,
+    currentUser.current.fetchNext,
+    currentUser.current.userId,
+    setUserId,
+    currentUser.current.hasFinished
+  );
 
-  if (isError) {
-    return <span>Error: {error.message}</span>;
-  }
-
-  const postContent = posts.map((post) => {
-    return <Post key={post._id} post={post} />;
+  const postContent = posts.map((post, i, postArray) => {
+    return (
+      <Post
+        key={post._id}
+        post={post}
+        ref={i === postArray.length - 1 ? lastPostRef : null}
+      />
+    );
   });
 
   return (
@@ -48,6 +86,8 @@ function NewFeed() {
       <div className="timeline">
         <div className="timeline-main">
           <main>{postContent}</main>
+          {isLoading && <span>Loading...</span>}
+          {isError && <span>Error: {error.message}</span>}
         </div>
       </div>
     </div>
