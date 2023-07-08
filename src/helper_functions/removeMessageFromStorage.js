7d8f23760d52e7a219c6ed65ff0b0c36bad9cd3f
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-07 22:21:31 -0400

    Return if messages is null

--- src/helper_functions/removeMessageFromStorage.js
+++ src/helper_functions/removeMessageFromStorage.js
@@ -1,6 +1,8 @@
 export default function removeMessageFromStorage(id) {
   const messages = JSON.parse(sessionStorage.getItem("messages"));
 
+  if (!messages) return;
+
   const updatedMessages = messages.filter((message) => {
     return message._id !== id;
   });
