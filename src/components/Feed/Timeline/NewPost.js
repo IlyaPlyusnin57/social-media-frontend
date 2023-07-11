1e78a64641a330f628a947060a4bb45ef1db7569
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-11 06:46:23 -0400

    Finish implementing tagging users
    Now the tag-drop-down list can be filtered and tagged users appear below the input
    
    Also add styles for the tagged users

--- src/components/Feed/Timeline/NewPost.js
+++ src/components/Feed/Timeline/NewPost.js
@@ -6,14 +6,17 @@ import { useRef, useState } from "react";
 import useAxiosConfig2 from "../../../api/useAxiosConfig2";
 import { getSubs } from "../../../apiCalls";
 import { useQuery } from "@tanstack/react-query";
+import ClearIcon from "@mui/icons-material/Clear";
 
 function NewPost({ setPosts }) {
   const { user, profile_picture, socket } = useAuth();
   const post_content = useRef(null);
   const api = useAxiosConfig2();
   const [subs, setSubs] = useState([]);
+  const allSubs = useRef([]);
+  const isSearchOn = useRef(true);
   const [queryEnabled, setQueryEnabled] = useState(false);
-  const taggedUsers = useRef([]);
+  const [taggedUsers, setTaggedUsers] = useState([]);
 
   useQuery({
     queryFn: () => getSubs(api, user),
@@ -21,6 +24,7 @@ function NewPost({ setPosts }) {
     enabled: queryEnabled,
     refetchOnWindowFocus: false,
     onSuccess: (users) => {
+      allSubs.current = users;
       setSubs(users);
     },
   });
@@ -41,11 +45,12 @@ function NewPost({ setPosts }) {
     try {
       const res = await api.post("posts/", {
         postObject,
-        taggedUsers: taggedUsers.current,
+        taggedUsers,
         sendUser: user,
       });
 
       if (res.status === 200) {
+        setTaggedUsers([]);
         const { newPost, tagObjects } = res.data;
 
         setPosts((prev) => [newPost, ...prev]);
@@ -59,23 +64,60 @@ function NewPost({ setPosts }) {
   }
 
   function handleInput() {
-    const lastChar = document.querySelector(".input-send").value.at(-1);
+    const value = document.querySelector(".input-send").value;
 
-    if (lastChar && lastChar === "@") {
-      setQueryEnabled(true);
-      document.querySelector(".tag-drop-down").classList.add("show");
+    // check if the input has all of the tagged usernames
+    taggedUsers.forEach((user) => {
+      if (!value.includes(user.username)) {
+        setTaggedUsers((prev) =>
+          prev.filter((taggedUser) => {
+            return taggedUser._id !== user._id;
+          })
+        );
+      }
+    });
+
+    const lastChar = value.at(-1);
+
+    if ((lastChar && lastChar === " ") || !value.includes("@")) {
+      isSearchOn.current = false;
+      setSubs([]);
     } else {
-      document.querySelector(".tag-drop-down").classList.remove("show");
+      isSearchOn.current = true;
+      setQueryEnabled(true);
+    }
+
+    if (isSearchOn.current) {
+      let index = value.lastIndexOf("@");
+      const query = value.substring(++index);
+      const arr = allSubs.current.filter((user) => {
+        return user.username.match(query);
+      });
+
+      setSubs(arr);
     }
   }
 
+  function removeTaggedUserFromInput(username) {
+    const value = document
+      .querySelector(".input-send")
+      .value.replace(`@${username}`, "");
+
+    document.querySelector(".input-send").value = value;
+    setTaggedUsers((prev) =>
+      prev.filter((user) => {
+        return user.username !== username;
+      })
+    );
+  }
+
   function tagUser(user) {
-    const username = `${user.first_name} ${user.last_name}`;
-    document.querySelector(".input-send").value += username;
-    taggedUsers.current.push(user);
-    // const sectionInput = document.querySelector("#new-post-input");
-    // sectionInput.innerHTML += `<span id="tagged-user">${user.first_name} ${user.last_name}</span>`;
-    document.querySelector(".tag-drop-down").classList.remove("show");
+    const value = document.querySelector(".input-send").value;
+    let index = value.lastIndexOf("@");
+    document.querySelector(".input-send").value = value.substring(0, ++index);
+    document.querySelector(".input-send").value += `${user.username}`;
+    setTaggedUsers((prev) => [...prev, user]);
+    setSubs([]);
   }
 
   return (
@@ -91,18 +133,36 @@ function NewPost({ setPosts }) {
           onInput={handleInput}
         />
 
-        <div className="tag-drop-down">
-          <ul>
-            {subs.map((user) => {
+        {subs.length > 0 && (
+          <div className="tag-drop-down">
+            <ul>
+              {subs.map((user) => {
+                return (
+                  <li
+                    key={user._id}
+                    onClick={() => tagUser(user)}
+                  >{`${user.first_name} ${user.last_name} (@${user.username})`}</li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+
+        <section id="tagged-users">
+          {Array.isArray(taggedUsers) &&
+            taggedUsers.length > 0 &&
+            taggedUsers.map((user) => {
               return (
-                <li
-                  key={user._id}
-                  onClick={() => tagUser(user)}
-                >{`${user.first_name} ${user.last_name}`}</li>
+                <span key={user._id} id="tagged-user">
+                  {user.username}
+                  <ClearIcon
+                    id="clear-icon"
+                    onClick={() => removeTaggedUserFromInput(user.username)}
+                  />
+                </span>
               );
             })}
-          </ul>
-        </div>
+        </section>
       </section>
       <SendIcon className="new-post-send-icon" onClick={handleSendIconClick} />
     </div>
