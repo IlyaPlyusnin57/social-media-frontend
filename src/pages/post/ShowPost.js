62fee5441c45337d5b613b519b9053190e6f0d71
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-13 11:00:00 -0400

    Import Post component
    Replace duplicate code with the Post component instead

--- src/pages/post/ShowPost.js
+++ src/pages/post/ShowPost.js
@@ -2,33 +2,21 @@ import { useLocation } from "react-router-dom";
 import { getPost } from "../../apiCalls";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosConfig2 from "../../api/useAxiosConfig2";
-import { useAuth } from "../../context/AuthContext";
-import { format } from "timeago.js";
-
-import Box from "@mui/material/Box";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import ShareIcon from "@mui/icons-material/Share";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import LikerList from "../../components/LikerList/LikerList";
 import { useState } from "react";
+import Post from "../../components/Feed/Timeline/Post";
 
 function ShowPost() {
   const { state: postId } = useLocation();
   const api = useAxiosConfig2();
-  const { user, profile_picture } = useAuth();
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-  const [showList, setShowList] = useState(false);
+  const [post, setPost] = useState([]);
 
-  const {
-    data: post,
-    isLoading,
-    isError,
-    error,
-  } = useQuery({
+  const { isLoading, isError, error } = useQuery({
     queryKey: [postId],
     queryFn: () => getPost(api, postId),
     refetchOnWindowFocus: false,
+    onSuccess: (data) => {
+      setPost((prev) => [...prev, data]);
+    },
   });
 
   if (!post) {
@@ -43,59 +31,19 @@ function ShowPost() {
     return <span>Error: {error.message}</span>;
   }
 
-  const isLiked = post?.likes.includes(user._id);
-
-  function addDislay() {
-    setShowList(true);
-  }
-
-  function removeDisplay() {
-    setShowList(false);
-  }
-
-  return (
-    <div className="post">
-      <div className="post-title">
-        <img src={profile_picture} alt="" />
-        <div className="post-info margin-left">
-          <div className="username">{user?.username}</div>
-          <div className="post-date">{format(post.createdAt)}</div>
+  const myPost = post.map((post) => {
+    if (post == null) {
+      return (
+        <div key={"unique-key"} className="post">
+          Post does not exist anymore
         </div>
-      </div>
+      );
+    }
 
-      <div className="content-wrapper" onMouseLeave={removeDisplay}>
-        <div className="post-content">
-          <div className="text-content">{post?.desc + " " + post.userId}</div>
-          {post?.img && (
-            <div className="img-content">
-              <img src={PF + post.img} alt="" />
-            </div>
-          )}
-        </div>
+    return <Post key={post._id} post={post} setPosts={setPost} />;
+  });
 
-        {showList && <LikerList {...{ removeDisplay, post }} />}
-
-        <div className="post-footer">
-          <Box className="icon-wrapper post-likes" onMouseEnter={addDislay}>
-            {isLiked ? (
-              <FavoriteIcon className="post-footer-icon red-heart-icon" />
-            ) : (
-              <FavoriteBorderIcon className="post-footer-icon" />
-            )}
-
-            <span>{post.likes.length}</span>
-          </Box>
-
-          {/* <Box className="icon-wrapper">
-            <ChatBubbleOutlineIcon className="post-footer-icon" />
-          </Box>
-          <Box className="icon-wrapper">
-            <ShareIcon className="post-footer-icon" />
-          </Box> */}
-        </div>
-      </div>
-    </div>
-  );
+  return myPost;
 }
 
 export default ShowPost;
