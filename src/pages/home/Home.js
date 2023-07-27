6a0acb75ae7ce70a1e944604a706fb64663dd969
Ilya Plyusnin <ilyaplyusnin57@gmail.com>
2023-07-27 16:57:47 -0400

    Resolve merge conflict

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,10 +3,6 @@ import Leftbar from "../../components/Leftbar/Leftbar";
 import "./home.scss";
 import SmallChat from "../../components/Chat/SmallChat";
 import useMediaQuery from "@mui/material/useMediaQuery";
-<<<<<<< HEAD
-=======
-import { useAuth } from "../../context/AuthContext";
->>>>>>> f9066ce7b110d2089bdc7487915c3473d147c9fa
 
 function Home({ children }) {
   const matchesMediaQuery = useMediaQuery("(min-width: 830px)");
